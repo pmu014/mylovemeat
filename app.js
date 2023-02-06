64542d25175a510be07d850e56b689ba7dec5050
hyowony <118158856+hyowony@users.noreply.github.com>
2023-02-07 01:29:56 +0900

    3계층,시퀄라이즈 적용x 마이페이지 기능 구현성공 70%

--- app.js
+++ app.js
@@ -5,77 +5,14 @@ const port = 3000
 const mypageRouter = require("./src/routes/mypage.js")
 const ejs = require("ejs")
 
-
-
-// const mysql = require('mysql2');  // mysql 모듈 로드
-// const conn = {  // mysql 접속 설정
-//     host: 'localhost',
-//     port: '3306',
-//     user: 'root',
-//     password: '0000',
-//     database: 'mylovemeat'
-// };
-
-// app.get("/mypage", (req, res) => { 
-//   res.render("mypage.ejs")
-// })
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src', 'views'));
 app.use(express.json());
 app.use("/api", mypageRouter);
 app.use(express.static(path.join(__dirname, 'src', 'public')));
 app.use('/node_modules', express.static(path.join(__dirname + '/node_modules')))
-// app.use(express.static(path.join(__dirname, 'src', 'public')));
-// app.set("view engine", "ejs"); //nunjucks를 사용하기 위한 express 세팅
-// app.set("views", "./views"); //첫번째 인자값 폴더명, 두번째 인자값 변수명 = views라는 폴더에서 ejs 관리한다는 의미
+
 app.engine("html", require("ejs").renderFile);
-
-
-// app.get("/", (req,res)=> {
-
-//     let connection = mysql.createConnection(conn); // DB 커넥션 생성
-//     connection.connect();   // DB 접속
-
-
-//     let testQuery = "SELECT * FROM users";
- 
-// connection.query(testQuery, function (err, results, fields) { // testQuery 실행
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(results);
-//     res.send(results)
-// });
-
-    
-//   res.send(results)
-// })
-
-// var connection = mysql.createConnection(conn); // DB 커넥션 생성
-// connection.connect();   // DB 접속
- 
-// var testQuery = "INSERT INTO `users` (`username`,`password`) VALUES ('test','test');";
- 
-// connection.query(testQuery, function (err, results, fields) { // testQuery 실행
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(results);
-// });
- 
-// const testQuery = "SELECT * FROM users";
- 
-// connection.query(testQuery, function (err, results, fields) { // testQuery 실행
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(results);
-// });
- 
- 
-// connection.end(); // DB 접속 종료
-
 
 app.listen(port, ()=> {
   console.log(port, "서버 정상 작동중")
--- src/routes/mypage.js
+++ src/routes/mypage.js
@@ -12,38 +12,45 @@ const conn = {  // mysql 접속 설정
 };
 
 
-
-
 router.get("/",(req,res)=> {
   res.send("첫페이지 ")
 });
 
 router.get("/users", (req,res)=> { 
-  
+ //유저가 로그인 되어있는 상태를 가정한다.
+ //토큰을 보냄 유저는 req할때 쿠키에 jwt을 담는다. header에 항상 쿠키가 담겨있다. 
+ //보통 토큰안에 pk가 담겨있다 가정을 하고 user.id를 추출함. 
+ // 
+ 
+ const userId = 1
+ //로그인 유저 pk =1 
+ //pk =1 가 마이페이지 
+ //mysql에서 findone 해서 where pk === 1 
+ //
 
   let connection = mysql.createConnection(conn); // DB 커넥션 생성
     connection.connect();   // DB 접속
 
-    let testQuery = "SELECT * FROM users";
+    let testQuery = "SELECT * FROM users where id = 1";
  
-connection.query(testQuery, function (err, results, fields) { // testQuery 실행
-    if (err) {
-        console.log(err);
-    }
-    console.log(results);
+    connection.query(testQuery, function (err, results, fields) { // testQuery 실행
+      
+    console.log("results,", results)
+    res.json({user:results[0]})
+    //객체만 가져옴 
+    //findall 유저목록, 다수를 가져올 때 쓴다. 배열에 묶여있음.
+    //findone은 객체만 가져옴 {}만 가져옴 
 
-    res.render("mypage")
+
+    // res.render("mypage")
 
   
   });
-  // res.send("마이페이지입니다.")
+  
 });
 
-// router.get("/test", (req, res) => { 
-//   res.render("mypage")
-// })
-
-// app.set('view engine', 'ejs');
-// app.set('views', path.join(__dirname, 'src', 'views'));
+router.get("/mypage", (req,res)=> {
+  res.render("mypage")
+})
 
 module.exports = router;
